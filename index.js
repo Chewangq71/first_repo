48789bfaba49ae01a385b653ba4d1f487444f490
abhiral chhetri <abhiralchhetri6@gmail.com>
2025-06-14 13:52:26 +0530

    COmmit

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const PORT = 4000;
 const path = require("path");
 
 
-
+app.use(express.json())
  app.set("views", path.join(__dirname, "./public/views")); 
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/hello", (req, res) => {
-  res.render("index"); 
+app.get("/login", (req, res) => {
+    console.log(req.body);
+    res.render('index' , {name: "Ouput"})
 });
 
 app.listen(PORT, (err) => {
