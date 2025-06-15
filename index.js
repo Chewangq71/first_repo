adf291e3e5295e04730c2622f8c4692c4f0ad956
chewang tamang <chewangtamang@chewangs-MacBook-Air.local>
2025-06-15 21:02:53 +0530

    YohO

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 const path = require("path");
-const cors = require("cors");
+const cors = require('cors');
 
 app.use(cors());
 
